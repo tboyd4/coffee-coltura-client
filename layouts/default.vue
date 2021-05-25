a9e4f2e2f1d23ae274dcc98d0b3c006eaabd89df
Tyler Boyd <tboyd.evan@gmail.com>
2021-05-24 20:09:52 -0500

    scaffolded out some pages, and added some new colors to the nuxt.config

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,22 @@
 <template>
   <v-app dark>
     <v-app-bar fixed app>
-      <v-toolbar-title>Coffee Coltura</v-toolbar-title>
+      <v-toolbar-title>
+        <v-btn to="/">Coffee Coltura</v-btn>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn outlined class="app-bar-button" color="primary" to="/brewguides"
+        >Brew Guides</v-btn
+      >
+      <v-btn outlined class="app-bar-button" color="primary" to="/beans"
+        >Beans</v-btn
+      >
+      <v-btn outlined class="app-bar-button" color="primary" to="/journal"
+        >Coffee Journal</v-btn
+      >
+      <v-btn outlined class="app-bar-button" color="primary" to="/settings"
+        >Settings</v-btn
+      >
     </v-app-bar>
     <v-main>
       <nuxt />
@@ -16,3 +31,13 @@
 <script>
 export default {}
 </script>
+
+<style lang="scss" scoped>
+span {
+  text-align: center;
+  margin: 0px auto;
+}
+.app-bar-button {
+  margin: 0px 5px;
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -51,13 +51,9 @@ export default {
       dark: true,
       themes: {
         dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
+          primary: colors.teal.darken1,
+          accent: colors.lightBlue.darken2,
+          secondary: colors.pink.darken1,
         },
       },
     },
